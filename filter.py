692504c2b0bc47ee4b8c8088ceea21a6ab42538d
Walter Chen <YPWALTER@WalterdeMacBook-Pro.local>
2015-08-06 17:32:26 +0800

    filter default to no tests

--- filter.py
+++ filter.py
@@ -22,24 +22,28 @@ args = parser.parse_args(sys.argv[1:])
 filters = {}
 cur = 0
 str = args.filters
-for i in range(len(str)):
-    if i != cur and (str[i] == "+" or str[i] == "-"):
-        filters[str[cur+1:i]] = (str[cur] == "+")
-        cur = i
-filters[str[cur+1:i+1]] = (str[cur] == "+")
-print filters
-
-
 selected = []
-for test in tests:
-    run = True
-    characterists = tests[test]
-    for c in characterists:
-        if filters.has_key(c):
-            if not filters[c]:
-                run = False
-                break
-    if run:
-        selected.append(test)
+if str is not None:
+    for i in range(len(str)):
+        if i != cur and (str[i] == "+" or str[i] == "-"):
+            filters[str[cur+1:i]] = (str[cur] == "+")
+            cur = i
+    filters[str[cur+1:i+1]] = (str[cur] == "+")
+    print filters
+
+    for test in tests:
+        true = True
+        characterists = tests[test]
+        if len(characterists) == 0:
+            true = False
+        has_key = False
+        for c in characterists:
+            if filters.has_key(c):
+                true = true & filters[c]
+                has_key = True
+        if true and has_key:
+            selected.append(test)
+else:
+   selected = tests.keys()
 
 print selected
